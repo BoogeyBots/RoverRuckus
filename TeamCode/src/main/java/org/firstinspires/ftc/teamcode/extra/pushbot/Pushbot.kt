7670081690becb29273e0ec371a8939f686d1130
Sergiu Marton <alexander.sergiu@gmail.com>
2018-10-27 12:07:37 +0300

    Add code documentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extra/pushbot/Pushbot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extra/pushbot/Pushbot.kt
@@ -1,24 +1,33 @@
 package org.firstinspires.ftc.teamcode.extra.pushbot
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.util.Range
 
+@TeleOp(name = "Pushbot", group = "PUSHBOT")
 class Pushbot : OpMode() {
+    /** The Hardware of the robot */
     private val hardware = PushbotHardware()
 
     override fun init() {
+        // Initialize the hardware (all the motors) using the TeleOp's hardware map
         hardware.init(hardwareMap)
     }
 
     override fun loop() {
+        /** How much does it want to move forward - using LT */
         val throttle: Double = gamepad1.left_trigger.toDouble()
+        /** How much does it want to move forward - using LT */
         val brake: Double = gamepad1.left_trigger.toDouble()
 
+        /** How much does it want move left-right - using the right stick's X axis */
         val horizontalMovement: Double = -gamepad1.right_stick_x.toDouble()
 
+        // Prevent overflow by clipping the values between +1 and -1
         hardware.leftMotorPower = Range.clip(throttle - brake + horizontalMovement, 1.0, -1.0)
         hardware.rightMotorPower = Range.clip(throttle - brake - horizontalMovement, 1.0, -1.0)
 
+        // Debugging data
         telemetry.addData("Movement:", "Forward: ${throttle + brake}, Sideways: $throttle")
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extra/pushbot/PushbotHardware.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/extra/pushbot/PushbotHardware.kt
@@ -5,15 +5,23 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.Servo
 
+/**
+ * This class holds all the objects that represent the robot's hardware (eg. motors)
+ */
 class PushbotHardware {
     private lateinit var leftMotor: DcMotor
     private lateinit var rightMotor: DcMotor
+
     private lateinit var armMotor: DcMotor
+
     lateinit var leftClawServo: Servo
     lateinit var rightClawServo: Servo
 
     var leftMotorPower: Double = 0.0
+        // Custom setters for caching system
         set(value) {
+            // Only set the power of the motor if the last's "frame" power is
+            // different from the current one
             if (value != field) {
                 field = value
                 leftMotor.power = field
@@ -36,7 +44,12 @@ class PushbotHardware {
             }
         }
 
+    /**
+     *  Initialize all the robot's components
+     *  @param hardwareMap The hardware map from an OpMode
+     */
     fun init(hardwareMap: HardwareMap) {
+        // Get the motors from the hardware map
         leftMotor = hardwareMap.get(DcMotor::class.java, "left_motor")
         rightMotor = hardwareMap.get(DcMotor::class.java, "right_motor")
         armMotor = hardwareMap.get(DcMotor::class.java, "arm_motor")
@@ -48,9 +61,11 @@ class PushbotHardware {
         rightMotor.power = 0.0
         armMotor.power = 0.0
 
+        // The motors have opposite directions to make the wheels run in the same direction
         leftMotor.direction = DcMotorSimple.Direction.FORWARD
         rightMotor.direction = DcMotorSimple.Direction.REVERSE
 
+        // Idk what an encoder is but Andrei said we don't use them
         leftMotor.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
         rightMotor.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
         armMotor.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
